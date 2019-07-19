7ed73a0e06eb327bc8814585fa9b4b73448ff4a0
ohlulu <ohlulu@goonsdesign.com>
2019-07-19 23:07:12 +0800

    [add] [base doa]

---
+++ kMoney/Model/Records.swift
@@ -0,0 +1,21 @@
+//
+//  Records.swift
+//  kMoney
+//
+//  Created by ohlulu on 2019/7/19.
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+@objc class Records: Object {
+    
+    dynamic var time = Date().timeIntervalSince1970
+    
+    dynamic var category = Category()
+    
+    dynamic var money = 0
+    
+    dynamic var tag = ""
+}
