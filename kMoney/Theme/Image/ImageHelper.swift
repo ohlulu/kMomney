27f8a8acc6ffffbadaf7712343ec1c82943c581c
ohlulu <ohlulu@goonsdesign.com>
2019-08-03 12:20:05 +0800

    [add] [image]

---
+++ kMoney/Theme/Image/ImageHelper.swift
@@ -0,0 +1,40 @@
+
+
+//
+//  OhImgae.swift
+//  ohlulu
+//
+//  Created by OhImgae.py on 2019/08/02
+//  Lastupdate on 2019/7/9
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+        
+import UIKit
+
+extension UIImage {
+
+    static var addPressed: UIImage {
+        return UIImage(named: "addPressed")!
+    }
+
+    static var settingSelected: UIImage {
+        return UIImage(named: "settingSelected")!
+    }
+
+    static var homeSelected: UIImage {
+        return UIImage(named: "homeSelected")!
+    }
+
+    static var addNormal: UIImage {
+        return UIImage(named: "addNormal")!
+    }
+
+    static var reportSelected: UIImage {
+        return UIImage(named: "reportSelected")!
+    }
+
+    static var navigationBarClose: UIImage {
+        return UIImage(named: "navigationBarClose")!
+    }
+
+}
