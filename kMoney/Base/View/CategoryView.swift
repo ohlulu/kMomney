7990c666032454cceadf36f81d2d37000510bb99
ohlulu <ohlulu@goonsdesign.com>
2019-08-20 23:35:11 +0800

    [add] [CategoryView] [file]

---
+++ kMoney/Base/View/CategoryView.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryView.swift
+//  kMoney
+//
+//  Created by ohlulu on 2019/8/20.
+//  Copyright © 2019 ohlulu. All rights reserved.
+//
+
+import UIKit
+
+class CategoryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
